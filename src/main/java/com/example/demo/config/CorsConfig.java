e5731555996a9d0ba3f32b6fe00df2d0fc376449
nguyenminhhuy12 <nguyenminhhuy9425@gmail.com>
2026-06-28 17:42:22 +0700

    cors

---
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                    .allowedOrigins(
+                        "http://localhost",
+                        "http://localhost:8080",
+                        "https://anbatocom-7ff5f.web.app",
+                        "https://anbatocom-7ff5f.firebaseapp.com"
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
